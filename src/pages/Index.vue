2120baf036f43b184d2785c13afbee1dea80b0d7
Akshit Sharma <akshit.s@outlook.com>
2021-10-11 13:11:22 +0530

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+<template>
+  <q-page class="row items-center justify-evenly">
+
+    <button @click="showDialog">showDialog</button>
+  </q-page>
+
+  <BaseDialog />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseDialog from 'src/components/BaseDialog.vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+
+  components: { BaseDialog },
+
+  methods: {
+    showDialog() {
+      this.emitter.emit('showDialog', true);
+    },
+  },
+});
+</script>
